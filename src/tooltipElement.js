9f8473398f50a009747a9b1ddd6c461fef830055
Ning <w1203771737@gmail.com>
2023-05-31 19:26:04 +0800

    add classname

--- src/tooltipElement.js
+++ src/tooltipElement.js
@@ -1,34 +1,33 @@
-'use strict';
+"use strict";
 
-var setStyle = require('./setStyle')
-var map      = {}
+var setStyle = require("./setStyle");
+var map = {};
 
-var result = function(config){
+var result = function (config) {
+  var element = map[config.id];
 
-    var element = map[config.id]
+  if (!element) {
+    element = setStyle(document.createElement("div"), config.style || {});
+    element.className = config.className;
 
-    if (!element){
-        element = setStyle(document.createElement('div'), config.style || {})
-        element.className = config.className
-
-        if (config.appendTooltip){
-            config.appendTooltip(element)
-        } else {
-            document.body.appendChild(element)
-        }
-        map[config.id] = element
+    if (config.appendTooltip) {
+      config.appendTooltip(element);
+    } else {
+      document.body.appendChild(element);
     }
+    map[config.id] = element;
+  }
 
-    return element
-}
+  return element;
+};
 
-result.destroy = function(config){
-	var element = map[config.id]
+result.destroy = function (config) {
+  var element = map[config.id];
 
-	if (element){
-		var parent = element.parentNode
-		parent && parent.removeChild(element)
-	}
-}
+  if (element) {
+    var parent = element.parentNode;
+    parent && parent.removeChild(element);
+  }
+};
 
-module.exports = result
+module.exports = result;
